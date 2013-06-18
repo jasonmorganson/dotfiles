8eb0ee000280b7aa0bae784cd0ddbc44b95cef94
Jason Morganson <jason@morganson.me>
2013-06-17 23:09:15 -0400

    Move file locations for freshshell

---
+++ config/awesome/debian/menu.lua
@@ -0,0 +1,101 @@
+-- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
+
+module("debian.menu")
+
+Debian_menu = {}
+
+Debian_menu["Debian_Applications_Accessibility"] = {
+	{"Xmag","xmag"},
+}
+Debian_menu["Debian_Applications_Editors"] = {
+	{"Xedit","xedit"},
+}
+Debian_menu["Debian_Applications_Graphics"] = {
+	{"ImageMagick","/usr/bin/display logo:","/usr/share/pixmaps/display.xpm"},
+	{"X Window Snapshot","xwd | xwud"},
+}
+Debian_menu["Debian_Applications_Network_Communication"] = {
+	{"Xbiff","xbiff"},
+}
+Debian_menu["Debian_Applications_Network_Web_Browsing"] = {
+	{"Google Chrome","/opt/google/chrome/google-chrome","/opt/google/chrome/product_logo_32.xpm"},
+}
+Debian_menu["Debian_Applications_Network"] = {
+	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
+	{ "Web Browsing", Debian_menu["Debian_Applications_Network_Web_Browsing"] },
+}
+Debian_menu["Debian_Applications_Programming"] = {
+	{"Python (v2.7)", "x-terminal-emulator -e ".."/usr/bin/python2.7","/usr/share/pixmaps/python2.7.xpm"},
+	{"Ruby (irb1.8)", "x-terminal-emulator -e ".."/usr/bin/irb1.8"},
+}
+Debian_menu["Debian_Applications_Science_Mathematics"] = {
+	{"Xcalc","xcalc"},
+}
+Debian_menu["Debian_Applications_Science"] = {
+	{ "Mathematics", Debian_menu["Debian_Applications_Science_Mathematics"] },
+}
+Debian_menu["Debian_Applications_Shells"] = {
+	{"Bash", "x-terminal-emulator -e ".."/bin/bash --login"},
+	{"Dash", "x-terminal-emulator -e ".."/bin/dash -i"},
+	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
+	{"Zsh", "x-terminal-emulator -e ".."/bin/zsh4"},
+}
+Debian_menu["Debian_Applications_System_Administration"] = {
+	{"Editres","editres"},
+	{"Xclipboard","xclipboard"},
+	{"Xfontsel","xfontsel"},
+	{"Xkill","xkill"},
+	{"Xrefresh","xrefresh"},
+}
+Debian_menu["Debian_Applications_System_Hardware"] = {
+	{"Xvidtune","xvidtune"},
+}
+Debian_menu["Debian_Applications_System_Monitoring"] = {
+	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
+	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
+	{"Xconsole","xconsole -file /dev/xconsole"},
+	{"Xev","x-terminal-emulator -e xev"},
+	{"Xload","xload"},
+}
+Debian_menu["Debian_Applications_System"] = {
+	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
+	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
+	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
+}
+Debian_menu["Debian_Applications_Terminal_Emulators"] = {
+	{"XTerm","xterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+	{"XTerm (Unicode)","uxterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+}
+Debian_menu["Debian_Applications_Viewers"] = {
+	{"Xditview","xditview"},
+}
+Debian_menu["Debian_Applications"] = {
+	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
+	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
+	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
+	{ "Network", Debian_menu["Debian_Applications_Network"] },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
+	{ "Science", Debian_menu["Debian_Applications_Science"] },
+	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
+	{ "System", Debian_menu["Debian_Applications_System"] },
+	{ "Terminal Emulators", Debian_menu["Debian_Applications_Terminal_Emulators"] },
+	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
+}
+Debian_menu["Debian_Games_Toys"] = {
+	{"Oclock","oclock"},
+	{"Xclock (analog)","xclock -analog"},
+	{"Xclock (digital)","xclock -digital -update 1"},
+	{"Xeyes","xeyes"},
+	{"Xlogo","xlogo"},
+}
+Debian_menu["Debian_Games"] = {
+	{ "Toys", Debian_menu["Debian_Games_Toys"] },
+}
+Debian_menu["Debian_Help"] = {
+	{"Xman","xman"},
+}
+Debian_menu["Debian"] = {
+	{ "Applications", Debian_menu["Debian_Applications"] },
+	{ "Games", Debian_menu["Debian_Games"] },
+	{ "Help", Debian_menu["Debian_Help"] },
+}
