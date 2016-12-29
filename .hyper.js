4838edcce211506f5caf665404ada1d728c545cb
Jason Morganson <jason@morganson.me>
2016-12-28 22:54:19 -0500

    Move Hyper config

---
+++ .hyper.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  config: {
+
+    shell: 'zsh',
+
+    fontSize: 10,
+
+    fontFamily: '"Input Mono", "Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+  },
+
+  plugins: [
+    'hyperterm-snazzy'
+  ]
+};
