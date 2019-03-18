e9e202a8c8b2a45be75dab9b1a26c181f8ef1418
Jason Morganson <jason@morganson.me>
2019-03-18 08:10:01 -0400

    Internalize snazzy theme

--- .hyper.js
+++ .hyper.js
@@ -1,3 +1,12 @@
+const foregroundColor = '#eff0eb'
+const backgroundColor = '#282a36'
+const red = '#ff5c57'
+const green = '#5af78e'
+const yellow = '#f3f99d'
+const blue = '#57c7ff'
+const magenta = '#ff6ac1'
+const cyan = '#9aedfe'
+
 module.exports = {
     config: {
         shell: 'zsh',
@@ -5,12 +14,36 @@ module.exports = {
         fontSize: 12,
 
         fontFamily:
-            '"Input Mono", "Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace'
+            '"Input Mono", "Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+
+        backgroundColor,
+        foregroundColor,
+        borderColor: '#222430',
+        cursorColor: '#97979b',
+        cursorAccentColor: backgroundColor,
+        selectionColor: 'rgba(151, 151, 155, 0.2)',
+        colors: {
+            black: backgroundColor,
+            red,
+            green,
+            yellow,
+            blue,
+            magenta,
+            cyan,
+            white: '#f1f1f0',
+            lightBlack: '#686868',
+            lightRed: red,
+            lightGreen: green,
+            lightYellow: yellow,
+            lightBlue: blue,
+            lightMagenta: magenta,
+            lightCyan: cyan,
+            lightWhite: foregroundColor
+        }
     },
 
     plugins: [
         'hyperlinks',
-        'hyper-snazzy',
         'hyperfullscreen',
         'hyperterm-paste',
         'hyper-hide-title',
