d1ab73fd00ee2efc5bafc0fad6d688bc282c47b6
Jason Morganson <jason@morganson.me>
2020-04-07 13:50:47 -0400

    Use /home/jmorganson

--- run_once_04_install_tmux_plugins.sh
+++ run_once_04_install_tmux_plugins.sh
@@ -2,7 +2,7 @@
 
 . ./.env
 
-export TMUX_PLUGIN_MANAGER_PATH=~/.tmux/plugins/
+TMUX_PLUGIN_MANAGER_PATH=$HOME/.tmux/plugins/
 
 # Install tpm
 git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER_PATH/tpm
