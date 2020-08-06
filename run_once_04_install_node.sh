e539066d175b88c243e0d70436d03bc5f743108a
Jason Morganson <jason@morganson.me>
2020-08-06 10:37:59 -0400

    Add plugins before install

--- run_once_04_install_node.sh
+++ run_once_04_install_node.sh
@@ -3,6 +3,7 @@
 . ./.env
 
 # FIXME: Installing node first as a workaround to having to import pgp keys
+asdf plugin add nodejs
 asdf install nodejs 13.0.1
 
 # Import the Node.js release team's OpenPGP keys to main keyring
--- run_once_04_install_python.sh
+++ run_once_04_install_python.sh
@@ -4,4 +4,5 @@
 
 # FIXME: Installing python first as a workaround to gcloud requiring it
 # SEE: https://github.com/jthegedus/asdf-gcloud/blob/master/bin/exec-env#L6
+asdf plugin add python
 asdf install python 3.6.8
