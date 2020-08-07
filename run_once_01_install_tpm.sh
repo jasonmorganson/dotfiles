fbced7b6904c7a9f887d301d8d6b1fce1c23a041
Jason Morganson <jason@cherre.com>
2020-08-07 08:02:33 -0400

    Simplify tpm install

--- run_once_01_install_tpm.sh
+++ run_once_01_install_tpm.sh
@@ -2,10 +2,5 @@
 
 . ./.env
 
-TMUX_PLUGIN_MANAGER_PATH=$HOME/.tmux/plugins/
-
-if [ ! -d $TMUX_PLUGIN_MANAGER_PATH/tpm ]
-then
-    # Install tpm
-    git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER_PATH/tpm
-fi
+# Install tpm
+[ -d ~/.tmux/plugins/tpm ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
