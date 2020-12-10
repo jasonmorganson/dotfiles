3f3151b9e7260214823dd3ff686d5cd395830257
Jason Morganson <jason@cherre.com>
2020-12-10 16:44:21 -0500

    Use install script in personal setup

--- SETUP.sh
+++ SETUP.sh
@@ -3,21 +3,4 @@
 export NAME="Jason Morganson"
 export EMAIL="jason@cherre.com"
 
-if ! command -v chezmoi > /dev/null
-then
-  curl -sfL https://git.io/chezmoi | sh
-  PATH=./bin:$PATH
-fi
-
-if [ -d ~/.local/share/chezmoi/.git ]
-then
-  chezmoi update --apply
-else
-  chezmoi init --apply https://github.com/jasonmorganson/dotfiles.git || chezmoi purge --force
-fi
-
-if [ -f ./bin/chezmoi ]
-then
-  rm ./bin/chezmoi
-  rmdir ./bin
-fi
+./install.sh
