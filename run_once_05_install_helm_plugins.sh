bd3216644360cc30719da017bd8adf93abd4fe4f
Jason Morganson <jason@cherre.com>
2020-12-28 16:25:20 -0500

    Update helm stable repo

--- run_once_05_install_helm_plugins.sh
+++ run_once_05_install_helm_plugins.sh
@@ -7,4 +7,4 @@ helm plugin install https://github.com/nouney/helm-gcs
 helm plugin install https://github.com/aslafy-z/helm-git
 helm plugin install https://github.com/databus23/helm-diff
 helm plugin install https://github.com/futuresimple/helm-secrets
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add stable https://charts.helm.sh/stable
