5842a4fe051afca032fa604d3fd19faf1b30ba71
Jason Morganson <jason@cherre.com>
2020-12-28 16:26:07 -0500

    Use active helm secrets fork

--- run_once_05_install_helm_plugins.sh
+++ run_once_05_install_helm_plugins.sh
@@ -6,5 +6,5 @@
 helm plugin install https://github.com/nouney/helm-gcs
 helm plugin install https://github.com/aslafy-z/helm-git
 helm plugin install https://github.com/databus23/helm-diff
-helm plugin install https://github.com/futuresimple/helm-secrets
+helm plugin install https://github.com/jkroepke/helm-secrets
 helm repo add stable https://charts.helm.sh/stable
