ee46798c60fe8cc23a682db87269e5af2d03f3a8
Jason Morganson <jason@cherre.com>
2020-12-29 12:11:35 -0500

    Update homebrew install to latest copy

--- run_once_01_install_brew.sh
+++ run_once_01_install_brew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install brew
-/bin/bash -c "CI=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "CI=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
