8a8f34dde01e8f20913ac74091d09bdc10ef518e
Jason Morganson <jason@cherre.com>
2020-12-28 19:08:18 -0500

    Use local dir for init

--- install.sh
+++ install.sh
@@ -16,3 +16,8 @@ if [ ! "$(command -v chezmoi)" ]; then
 else
   chezmoi=chezmoi
 fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
