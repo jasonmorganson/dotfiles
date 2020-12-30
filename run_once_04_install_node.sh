a8eaf8e6db97503a2c67ff24cc3a1b5fcbf5abf3
Jason Morganson <jason@cherre.com>
2020-12-30 15:41:08 -0500

    Revert "Install latest"
    This reverts commit 3e1669c2686e856685b8ddbe65f52238ff381371.

--- run_once_04_install_node.sh
+++ run_once_04_install_node.sh
@@ -4,7 +4,7 @@
 
 # FIXME: Installing node first as a workaround to having to import pgp keys
 asdf plugin add nodejs
-asdf install nodejs latest
+asdf install nodejs 13.0.1
 
 # Import the Node.js release team's OpenPGP keys to main keyring
 # SEE: https://github.com/asdf-vm/asdf-nodejs/issues/138
