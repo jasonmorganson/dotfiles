da41fae716d307ce482b9557adb49c817664383c
Jason Morganson <jason@cherre.com>
2020-12-30 09:16:03 -0500

    Install essential bundles

--- run_03_install_brew_bundles.sh
+++ run_03_install_brew_bundles.sh
@@ -2,5 +2,20 @@
 
 . ./.env
 
+# Essential bundles
+bundles="
+asdf
+getantibody/tap/antibody
+helm
+nvim
+tmux
+"
+
+# Ensure essential bundles are installed
+for bundle in $bundles
+do
+    brew install $bundle
+done
+
 # Install brew bundles
 brew bundle install
