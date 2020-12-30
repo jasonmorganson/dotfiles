8e769dcc7d5bef9f3072772a991e058f2bb8629e
Jason Morganson <jason@cherre.com>
2020-12-30 15:28:22 -0500

    Push python requirements into python install script

--- run_once_04_install_python.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. ./.profile
-
-# FIXME: Installing python first as a workaround to gcloud requiring it
-# SEE: https://github.com/jthegedus/asdf-gcloud/blob/master/bin/exec-env#L6
-asdf plugin add python
-asdf install python 3.6.8
