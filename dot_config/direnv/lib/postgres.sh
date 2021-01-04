b44fcc422b2702ec6bf90d624da5d8d1802d0c4d
Jason Morganson <jason@cherre.com>
2021-01-04 16:59:53 -0500

    Move postgres env into direnv

---
+++ dot_config/direnv/lib/postgres.sh
@@ -0,0 +1,5 @@
+PGHOST=localhost
+PSQLRC="$XDG_CONFIG_HOME/pg/psqlrc"
+PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass"
+PGSERVICEFILE="$XDG_CONFIG_HOME/pg/pg_service.conf"
+PSQL_HISTORY="$XDG_CACHE_HOME/pg/psql_history"
