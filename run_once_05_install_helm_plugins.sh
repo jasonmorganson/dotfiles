13c91c92bf1315973f7b8998ece69394f020f934
Jason Morganson <jason@cherre.com>
2021-01-07 14:51:40 -0500

    Comment on helm repo add

--- run_once_05_install_helm_plugins.sh
+++ run_once_05_install_helm_plugins.sh
@@ -2,9 +2,11 @@
 
 . ./.profile
 
+# Add stable repo
+helm repo add stable https://charts.helm.sh/stable
+
 # Install helm plugins
 helm plugin install https://github.com/nouney/helm-gcs
 helm plugin install https://github.com/aslafy-z/helm-git
 helm plugin install https://github.com/databus23/helm-diff
 helm plugin install https://github.com/jkroepke/helm-secrets
-helm repo add stable https://charts.helm.sh/stable
