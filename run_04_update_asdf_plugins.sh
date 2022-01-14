c5c50cad193d5290e8b1998670a3aa5bedfe95d1
Jason Morganson <jason@cherre.com>
2022-01-14 16:34:20 -0500

    feat(asdf): Use new plugin update command

--- run_04_update_asdf_plugins.sh
+++ run_04_update_asdf_plugins.sh
@@ -2,12 +2,4 @@
 
 . ./.profile
 
-while read plugin version
-do
-    if asdf plugin list | grep --silent $plugin
-    then
-        asdf plugin update $plugin
-    else
-        asdf plugin add $plugin
-    fi
-done < ~/.tool-versions
+asdf plugin update --all
