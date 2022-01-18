61047e3445c93e56a2bd767961b9600e8f93325a
Jason Morganson <jason@cherre.com>
2022-01-17 21:19:01 -0500

    Succeed if plugin is already installed

--- run_once_06_install_helm_plugins.sh
+++ run_once_06_install_helm_plugins.sh
@@ -2,11 +2,17 @@
 
 . ./.profile
 
+helm_plugin_install() {
+    # Exit code 1 indicates the plugin is already installed so test and exit
+    # successfully if that is the error code.
+    helm plugin install $1 || test $? = 1
+}
+
 # Add stable repo
 helm repo add stable https://charts.helm.sh/stable
 
 # Install helm plugins
-helm plugin install https://github.com/nouney/helm-gcs
-helm plugin install https://github.com/aslafy-z/helm-git
-helm plugin install https://github.com/databus23/helm-diff
-helm plugin install https://github.com/jkroepke/helm-secrets
+helm_plugin_install https://github.com/nouney/helm-gcs
+helm_plugin_install https://github.com/aslafy-z/helm-git
+helm_plugin_install https://github.com/databus23/helm-diff
+helm_plugin_install https://github.com/jkroepke/helm-secrets
