cda516d378a0acc70f6bd06af801be426be9610c
Jason Morganson <jason@cherre.com>
2022-02-15 17:48:23 +0000

    Add doc link

--- run_once_01_install_brew.sh
+++ run_once_01_install_brew.sh
@@ -3,4 +3,5 @@
 set -eu
 
 # Install brew
+# SEE: https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
