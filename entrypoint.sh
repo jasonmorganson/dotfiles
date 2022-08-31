4bcb7dff73bd15023fa4966d1fd1f1704c5621ae
Jason Morganson <jason@cherre.com>
2022-08-31 16:30:55 -0400

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+uid=1000
+gid=1000
+base_user="$(id --user --name $uid)"
+base_group="$(id --group --name $uid)"
+base_home="$(eval echo "~$base_user")"
+user="${USER:-$base_user}"
+group="$user"
+home="/home/$user"
+shell="${SHELL:-/home/linuxbrew/.linuxbrew/bin/zsh}"
+
+sudo groupmod --gid "$gid" --new-name "$group" "$base_group"
+sudo usermod --non-unique --gid "$gid" --uid "$uid" --home "$home" --shell "$shell" --login "$user" "$base_user"
+sudo ln --symbolic --no-dereference "$base_home" "$home"
+sudo chown --no-dereference "$user" "$home"
+sudo --preserve-env --set-home --shell --user="$user"
