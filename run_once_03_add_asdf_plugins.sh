20a4a183642de663f32f07aed4a96d5cac2f53f3
Jason Morganson <jason@cherre.com>
2022-11-05 15:45:58 -0400

    Sort asdf plugins

--- run_once_03_add_asdf_plugins.sh
+++ run_once_03_add_asdf_plugins.sh
@@ -16,13 +16,13 @@ asdf_plugin_add helmfile
 asdf_plugin_add krew
 asdf_plugin_add kubectl
 asdf_plugin_add kubectx
+asdf_plugin_add kustomize
 asdf_plugin_add nodejs
 asdf_plugin_add poetry
 asdf_plugin_add postgres
 asdf_plugin_add python
 asdf_plugin_add shellcheck
 asdf_plugin_add shfmt
-asdf_plugin_add kustomize
 asdf_plugin_add skaffold
 asdf_plugin_add sops
 asdf_plugin_add yq
