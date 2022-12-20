5075bf40775829718feb79b7100d68eddea124e8
Jason Morganson <jason@cherre.com>
2022-12-19 19:17:06 -0500

    Add asdf plugins

--- run_once_03_add_asdf_plugins.sh
+++ run_once_03_add_asdf_plugins.sh
@@ -8,15 +8,26 @@ asdf_plugin_add() {
     asdf plugin add "$1" "${2:-}" || test $? = 2
 }
 
+asdf_plugin_add ag
+asdf_plugin_add chezmoi
 asdf_plugin_add deno
 asdf_plugin_add direnv
+asdf_plugin_add exa
+asdf_plugin_add fzf
+asdf_plugin_add gcloud
+asdf_plugin_add git
 asdf_plugin_add golang
+asdf_plugin_add hasura-cli
 asdf_plugin_add helm
 asdf_plugin_add helmfile
+asdf_plugin_add hub
+asdf_plugin_add k9s
 asdf_plugin_add krew
 asdf_plugin_add kubectl
 asdf_plugin_add kubectx
+asdf_plugin_add kubie
 asdf_plugin_add kustomize
+asdf_plugin_add neovim
 asdf_plugin_add nodejs
 asdf_plugin_add poetry
 asdf_plugin_add postgres
@@ -26,6 +37,8 @@ asdf_plugin_add shellcheck
 asdf_plugin_add shfmt
 asdf_plugin_add skaffold
 asdf_plugin_add sops
+asdf_plugin_add tmux
+asdf_plugin_add vim
 asdf_plugin_add yq
 
 asdf_plugin_add gcloud https://github.com/jasonmorganson/asdf-gcloud.git
