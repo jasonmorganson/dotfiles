088165a0a61ffb1d59bc18b0fa399476931fa132
Jason Morganson <jason@morganson.me>
2023-04-05 15:42:37 -0400

    Remove unused asdf plugins

--- run_once_03_add_asdf_plugins.sh
+++ run_once_03_add_asdf_plugins.sh
@@ -10,22 +10,10 @@ asdf_plugin_add() {
 
 asdf_plugin_add deno
 asdf_plugin_add direnv
-asdf_plugin_add golang
-asdf_plugin_add helm
-asdf_plugin_add helmfile
-asdf_plugin_add krew
-asdf_plugin_add kubectl
-asdf_plugin_add kubectx
-asdf_plugin_add kustomize
 asdf_plugin_add nodejs
-asdf_plugin_add poetry
-asdf_plugin_add postgres
 asdf_plugin_add python
 asdf_plugin_add rust
-asdf_plugin_add shellcheck
-asdf_plugin_add shfmt
 asdf_plugin_add skaffold
-asdf_plugin_add sops
 asdf_plugin_add yq
 
 asdf_plugin_add gcloud https://github.com/jasonmorganson/asdf-gcloud.git
