28859d0f038c76adc21a16f20a229d52d415283c
Jason Morganson <jason@morganson.me>
2023-04-06 10:58:13 -0400

    Rename and add rtx links

--- run_06_links.sh
+++ run_06_links.sh
@@ -5,3 +5,6 @@ set -eu
 # Link teas
 sudo ln -sf ~/.tea/tea.xyz/v0/bin/tea /usr/local/bin/tea
 sudo ln -sf ~/.tea/nushell.sh/v0.77/bin/nu /usr/local/bin/nu
+
+# Link rtxs
+sudo ln -sf ~/.local/share/rtx/installs/zellij/0.35.2/bin/zellij /usr/local/bin/zellij
