95ae357c7e78d5ee98f9050bcea5d5ca38b0a772
Jason Morganson <jason@morganson.me>
2023-04-06 15:33:04 -0400

    Link helix

--- run_once_06_links.sh
+++ run_once_06_links.sh
@@ -5,6 +5,7 @@ set -eu
 # Link teas
 sudo ln -sf ~/.tea/tea.xyz/v0/bin/tea /usr/local/bin/tea
 sudo ln -sf ~/.tea/nushell.sh/v0.77/bin/nu /usr/local/bin/nu
+sudo ln -sf ~/.tea/helix-editor.com/v23.3.0/bin/hx /usr/local/bin/hx
 
 # Link rtxs
 sudo ln -sf ~/.local/share/rtx/installs/zellij/0.35.2/bin/zellij /usr/local/bin/zellij
