d59ddeab15b70e63c5e2465f1c25b516a891a9df
Jason Morganson <jason@morganson.me>
2023-04-06 17:35:04 -0400

    Drop vim/neovim

--- run_05_install_vim_plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-. ./.profile
-set -eu
-
-# Install vim plugins
-nvim --headless +PlugInstall +qall
--- run_once_02_install_essential_brew_bundles.sh
+++ run_once_02_install_essential_brew_bundles.sh
@@ -5,5 +5,4 @@ set -eu
 # Ensure essential bundles are installed
 brew bundle --file=/dev/stdin << EOF
 brew "git"
-brew "nvim"
 EOF
