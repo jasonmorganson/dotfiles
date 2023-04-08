1cbd631368fa919d522420adbdce8f0bf0d3c3a4
Jason Morganson <jasonmorganson@gmail.com>
2023-04-08 08:44:08 -0400

    Skip brew install

--- run_once_01_install_brew.sh
+++ run_once_01_install_brew.sh
@@ -4,4 +4,4 @@ set -eu
 
 # Install brew
 # SEE: https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
