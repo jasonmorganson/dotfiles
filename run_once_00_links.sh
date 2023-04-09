02fd18d53462671cc5f9101f1411e404d7e9ea0e
Jason Morganson <jason@morganson.me>
2023-04-09 14:15:22 -0400

    Link starship

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -6,6 +6,7 @@ set -eu
 sudo ln -sf ~/.tea/tea.xyz/v0/bin/tea /usr/local/bin/tea
 sudo ln -sf ~/.tea/nushell.sh/v0.77/bin/nu /usr/local/bin/nu
 sudo ln -sf ~/.tea/helix-editor.com/v23.3.0/bin/hx /usr/local/bin/hx
+sudo ln -sf ~/.tea/starship.rs/v1.13.1/bin/starship /usr/local/bin/starship
 
 # Link rtxs
 sudo ln -sf ~/.local/share/rtx/installs/zellij/0.35.2/bin/zellij /usr/local/bin/zellij
