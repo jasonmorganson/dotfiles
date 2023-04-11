2144b7ac214773669580503ab197a6953fc281bc
Jason Morganson <jason@morganson.me>
2023-04-11 16:21:05 -0400

    Add FIXME note to zlib link

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -4,7 +4,9 @@ set -eu
 
 # Link teas
 sudo ln -sf "~/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
-sudo ln -sf ~/.tea/zlib.net/v1.2.13/lib/libz.1.2.13.dylib /usr/local/lib/libz.1.2.13.dylib
 
 # Link rtxs
 sudo ln -sf ~/.local/share/rtx/installs/zellij/latest/bin/zellij /usr/local/bin/zellij
+
+# FIXME: Needed for delta as a workaround for cargo install
+sudo ln -sf ~/.tea/zlib.net/v1.2.13/lib/libz.1.2.13.dylib /usr/local/lib/libz.1.2.13.dylib
