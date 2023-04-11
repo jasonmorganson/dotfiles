40e17a40a0ee5c5c6f08314dfff324df993662e1
Jason Morganson <jason@morganson.me>
2023-04-11 16:20:39 -0400

    Use latest versions in links

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -3,8 +3,8 @@
 set -eu
 
 # Link teas
-sudo ln -sf ~/.tea/nushell.sh/v0.77/bin/nu /usr/local/bin/nu
+sudo ln -sf "~/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
 sudo ln -sf ~/.tea/zlib.net/v1.2.13/lib/libz.1.2.13.dylib /usr/local/lib/libz.1.2.13.dylib
 
 # Link rtxs
-sudo ln -sf ~/.local/share/rtx/installs/zellij/0.35.2/bin/zellij /usr/local/bin/zellij
+sudo ln -sf ~/.local/share/rtx/installs/zellij/latest/bin/zellij /usr/local/bin/zellij
