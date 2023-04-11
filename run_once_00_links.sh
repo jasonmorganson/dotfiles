90d6bd915fcfbf2c463db3b2fb7d5d5763dfaae1
Jason Morganson <jason@morganson.me>
2023-04-11 16:01:16 -0400

    Remove local bins

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -3,10 +3,7 @@
 set -eu
 
 # Link teas
-sudo ln -sf ~/.tea/tea.xyz/v0/bin/tea /usr/local/bin/tea
 sudo ln -sf ~/.tea/nushell.sh/v0.77/bin/nu /usr/local/bin/nu
-sudo ln -sf ~/.tea/helix-editor.com/v23.3.0/bin/hx /usr/local/bin/hx
-sudo ln -sf ~/.tea/starship.rs/v1.13.1/bin/starship /usr/local/bin/starship
 sudo ln -sf ~/.tea/zlib.net/v1.2.13/lib/libz.1.2.13.dylib /usr/local/lib/libz.1.2.13.dylib
 
 # Link rtxs
