5ebbfc9c0111557ffcc4f00b17b7cb7d3d7c9b58
Jason Morganson <jason@morganson.me>
2023-04-13 15:51:39 -0400

    fixup! Switch zellij link to tea

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -4,5 +4,5 @@ set -eu
 
 # Link teas
 sudo ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
-sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/nu" /usr/local/bin/nu
+sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 
