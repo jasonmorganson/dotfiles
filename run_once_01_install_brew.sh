8c0980782120e1dece126ea0863f600ee5954376
Jason Morganson <jason@morganson.me>
2023-04-14 20:09:24 -0400

    Drop brew

--- run_once_01_install_brew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. ./.profile
-set -eu
-
-# Install brew
-# SEE: https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux
-#NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
