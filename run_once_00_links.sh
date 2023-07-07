841e5e11f6a05cc21fa278f85023c857fc440fe7
Jason Morganson <jasonmorganson@gmail.com>
2023-07-07 12:57:28 +0000

    Make sure tea local bins are linked

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -6,3 +6,6 @@ set -eu
 sudo ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
 sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 
+mkdir -p "$HOME/.tea/.local/bin"
+ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" "$HOME/.tea/.local/bin/nu"
+ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" "$HOME/.tea/.local/bin/zellij"
