e04cb39358e105e0f05068943b9b7c8837f3ba1a
Jason Morganson <jasonmorganson@gmail.com>
2023-07-07 13:15:44 +0000

    Make sure tea is in local bin

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -7,5 +7,6 @@ sudo ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
 sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 
 mkdir -p "$HOME/.tea/.local/bin"
+ln -sf "$HOME/.tea/tea.xyz/v*/bin/tea" "$HOME/.tea/.local/bin/tea"
 ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" "$HOME/.tea/.local/bin/nu"
 ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" "$HOME/.tea/.local/bin/zellij"
