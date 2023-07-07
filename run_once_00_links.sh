6994b6705aa92ac822b5662ca1a6f69969117b0f
Jason Morganson <jasonmorganson@gmail.com>
2023-07-07 13:21:58 +0000

    Comment why linking is needed

--- run_once_00_links.sh
+++ run_once_00_links.sh
@@ -2,10 +2,12 @@
 . ./.profile
 set -eu
 
-# Link teas
+# Link tea bins to usr local so Alacritty can use the known path
 sudo ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
 sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 
+# Link the tea bins to the local tea bin path as a workaround to tea not doing this
+# automatically when running as root
 mkdir -p "$HOME/.tea/.local/bin"
 ln -sf "$HOME/.tea/tea.xyz/v*/bin/tea" "$HOME/.tea/.local/bin/tea"
 ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" "$HOME/.tea/.local/bin/nu"
