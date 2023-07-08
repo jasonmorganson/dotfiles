346da075f621fe2e9efa9d3749eb9da6837b022a
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-07-07 21:35:24 -0400

    Link tea command

--- run_once_after_03_links.sh
+++ run_once_after_03_links.sh
@@ -10,5 +10,5 @@ sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 # automatically when running as root
 mkdir -p "$HOME/.tea/.local/bin"
 ln -sf "$HOME/.tea/tea.xyz/v*/bin/tea" "$HOME/.tea/.local/bin/tea"
-ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" "$HOME/.tea/.local/bin/nu"
-ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" "$HOME/.tea/.local/bin/zellij"
+ln -sf tea "$HOME/.tea/.local/bin/nu"
+ln -sf tea "$HOME/.tea/.local/bin/zellij"
