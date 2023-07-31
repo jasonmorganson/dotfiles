e5ec01b998bd81efb72f8aab6da89154dd8a1a53
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-07-31 23:34:29 +0000

    Remove root links workaround

--- run_once_after_03_links.sh
+++ run_once_after_03_links.sh
@@ -6,9 +6,3 @@ set -eu
 sudo ln -sf "$HOME/.tea/nushell.sh/v*/bin/nu" /usr/local/bin/nu
 sudo ln -sf "$HOME/.tea/crates.io/zellij/v*/bin/zellij" /usr/local/bin/zellij
 
-# Link the tea bins to the local tea bin path as a workaround to tea not doing this
-# automatically when running as root
-mkdir -p "$HOME/.tea/.local/bin"
-ln -sf "$HOME/.tea/tea.xyz/v*/bin/tea" "$HOME/.tea/.local/bin/tea"
-ln -sf tea "$HOME/.tea/.local/bin/nu"
-ln -sf tea "$HOME/.tea/.local/bin/zellij"
