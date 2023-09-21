b765769adadda17fb3123afa38b6a4598a456f2d
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-09-21 12:50:54 -0400

    Follow link

--- .chezmoiscripts/run_once_01_install_tea.sh
+++ .chezmoiscripts/run_once_01_install_tea.sh
@@ -2,4 +2,4 @@
 set -eu
 
 # Install tea
-curl https://github.com/teaxyz/setup/releases/download/v0.18.3/install.sh | TEA_YES=1 SHELL=bash sh
+curl -L https://github.com/teaxyz/setup/releases/download/v0.18.3/install.sh | TEA_YES=1 SHELL=bash sh
