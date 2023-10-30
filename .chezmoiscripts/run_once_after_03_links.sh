7a2a512a6b0d8bee1836e4f13707fea2b378154a
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-10-30 18:29:09 +0000

    Update links

--- .chezmoiscripts/run_once_after_03_links.sh
+++ .chezmoiscripts/run_once_after_03_links.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-# Link tea bins to usr local so Alacritty can use the known path
-sudo ln -sf "$HOME/.tea/nushell.sh/v0/bin/nu" /usr/local/bin/nu
-sudo ln -sf "$HOME/.tea/crates.io/zellij/v0/bin/zellij" /usr/local/bin/zellij
+# Link pkgx bins to usr local so Alacritty can use the known path
+sudo ln -sf "$HOME/.pkgx/nushell.sh/v0/bin/nu" /usr/local/bin/nu
+sudo ln -sf "$HOME/.pkgx/crates.io/zellij/v0/bin/zellij" /usr/local/bin/zellij
