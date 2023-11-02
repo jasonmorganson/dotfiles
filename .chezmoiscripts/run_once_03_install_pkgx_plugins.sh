62752ccfe9d6edd9177f19e9b2456ebbeb4945e1
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-11-02 21:17:52 +0000

    Lock versions

--- .chezmoiscripts/run_once_03_install_pkgx_plugins.sh
+++ .chezmoiscripts/run_once_03_install_pkgx_plugins.sh
@@ -2,17 +2,17 @@
 set -eu
 
 # Install pkgx plugins
-pkgx install chezmoi.io
-pkgx install cli.github.com
-pkgx install crates.io/git-delta
-pkgx install crates.io/gitui
-pkgx install crates.io/ripgrep
-pkgx install crates.io/vivid
-pkgx install crates.io/zellij
-pkgx install git-scm.org
-pkgx install github.com/junegunn/fzf
-pkgx install github.com/peltoche/lsd
-pkgx install helix-editor.com
-pkgx install nushell.sh
-pkgx install starship.rs
+pkgx install chezmoi.io~2.34
+pkgx install cli.github.com^2.26
+pkgx install crates.io/git-delta^0.15
+pkgx install crates.io/gitui@latest
+pkgx install crates.io/ripgrep@latest
+pkgx install crates.io/vivid^0.9
+pkgx install crates.io/zellij~0.38
+pkgx install git-scm.org^2.40
+pkgx install github.com/junegunn/fzf@latest
+pkgx install github.com/peltoche/lsd^1
+pkgx install helix-editor.com~23.10
+pkgx install nushell.sh~0.86
+pkgx install starship.rs~1.16
 
