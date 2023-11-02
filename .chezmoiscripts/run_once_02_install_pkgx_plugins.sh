ad87652516967c9a661f25015076bee25db8f56a
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-11-02 21:57:04 +0000

    Only install commands when it is not already installed

--- .chezmoiscripts/run_once_02_install_pkgx_plugins.sh
+++ .chezmoiscripts/run_once_02_install_pkgx_plugins.sh
@@ -3,13 +3,13 @@ set -eu
 
 # Install pkgx plugins
 command -v "chezmoi" >/dev/null 2>&1 || pkgx install chezmoi.io~2.34
-pkgx install cli.github.com^2.26
+command -v "gh" >/dev/null 2>&1 || pkgx install cli.github.com^2.26
 pkgx install crates.io/git-delta^0.15
 pkgx install crates.io/gitui@latest
 pkgx install crates.io/ripgrep@latest
 pkgx install crates.io/vivid^0.9
 pkgx install crates.io/zellij~0.38
-pkgx install git-scm.org^2.40
+command -v "git" >/dev/null 2>&1 || pkgx install git-scm.org^2.40
 pkgx install github.com/junegunn/fzf@latest
 pkgx install github.com/peltoche/lsd^1
 pkgx install helix-editor.com~23.10
