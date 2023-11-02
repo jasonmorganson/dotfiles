f642190a2d9d4833ad7efa9aa54fbd17655e4c80
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-11-02 21:00:00 +0000

    Remove links

--- .chezmoiscripts/run_once_after_03_links.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-# Link pkgx bins to usr local so Alacritty can use the known path
-sudo ln -sf "$HOME/.pkgx/nushell.sh/v0/bin/nu" /usr/local/bin/nu
-sudo ln -sf "$HOME/.pkgx/crates.io/zellij/v0/bin/zellij" /usr/local/bin/zellij
