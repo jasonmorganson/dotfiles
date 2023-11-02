36906b4511bde693ac2573e940eb28eb6e56c425
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2023-11-02 20:49:48 +0000

    Install pkgx packages

---
+++ .chezmoiscripts/run_once_03_install_pkgx_plugins.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+# Install pkgx plugins
+pkgx install chezmoi.io
+pkgx install cli.github.com
+pkgx install crates.io/git-delta
+pkgx install crates.io/gitui
+pkgx install crates.io/ripgrep
+pkgx install crates.io/vivid
+pkgx install crates.io/zellij
+pkgx install git-scm.org
+pkgx install github.com/junegunn/fzf
+pkgx install github.com/peltoche/lsd
+pkgx install helix-editor.com
+pkgx install nushell.sh
+pkgx install starship.rs
+
