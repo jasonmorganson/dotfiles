41db45a093347bd9892173a1f42081a4bc3d8ac4
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2024-03-21 18:27:08 -0400

    Ignore git install errors

--- .chezmoiscripts/run_once_02_install_pkgx_plugins.sh
+++ .chezmoiscripts/run_once_02_install_pkgx_plugins.sh
@@ -11,7 +11,7 @@ pkgx install crates.io/gitui@latest
 pkgx install crates.io/ripgrep@latest
 pkgx install crates.io/vivid^0.9
 pkgx install crates.io/zellij~0.38
-command -v "git" >/dev/null 2>&1 || pkgx install git-scm.org^2.40
+pkgx install git-scm.org^2.40 || true
 pkgx install github.com/junegunn/fzf@latest
 pkgx install github.com/peltoche/lsd^1
 pkgx install helix-editor.com~23.10
