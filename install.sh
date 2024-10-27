cfae403716b02b05e45db4d980e9918183410ac9
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2024-10-27 12:45:12 -0400

    no-tty

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+set -- init --apply --source="${script_dir}" --no-tty
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
