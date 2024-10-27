2750f519bf72766ddcd72c2568b42dc2577aa7a2
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2024-10-27 10:53:13 -0400

    Try script again

--- install.sh
+++ install.sh
@@ -23,8 +23,6 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
-
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+exec script -q -c "$chezmoi init --apply --source=\"${script_dir}\""
