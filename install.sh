ef53b27177ae53072c8eb3f41907f6856e0a53ad
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2024-10-28 09:11:25 -0400

    Force install

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}" --no-tty
+set -- init --apply --source="${script_dir}" --no-tty --force
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
