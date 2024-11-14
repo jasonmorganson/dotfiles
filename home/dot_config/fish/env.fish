c777f087ad1534b541d902c37797c46ba440c629
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2024-11-14 15:58:44 -0500

    Drop env hacks

--- home/dot_config/fish/env.fish
+++
@@ -1,9 +0,0 @@
-for line in (sh -c "source ~/.profile && env" | grep '=')
-    set --local env (string split '=' $line)
-    set --local name $env[1]
-    set --local value $env[2]
-
-    if not contains -- $name PWD SHLVL _
-        set -x $name $value
-    end
-end
--- home/dot_config/nushell/env.nu
+++ home/dot_config/nushell/env.nu
@@ -1,10 +1,1 @@
 source ~/.config/nushell/default_env.nu
-
-sh -c $". ($env.HOME)/.profile && env"
-    | lines
-    | parse "{name}={value}"
-    | transpose --header-row
-    | into record
-    | reject --ignore-errors PWD FILE_PWD CURRENT_FILE
-    | load-env
-
