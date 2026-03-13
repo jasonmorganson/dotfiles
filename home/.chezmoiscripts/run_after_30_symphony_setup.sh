e34deff9993bd53348e982a96f1af5d0b893b173
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-03-13 14:34:27 -0400

    Simplify Symphony chezmoi hook

--- home/.chezmoiscripts/run_after_30_symphony_setup.sh
+++ home/.chezmoiscripts/run_after_30_symphony_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-cd "${HOME}/.local/share/symphony/elixir" 2>/dev/null || exit 0
+cd "${HOME}/.local/share/symphony/elixir"
 
-~/.local/bin/mise install
-~/.local/bin/mise exec -- mix setup
-~/.local/bin/mise exec -- mix build
+mise install
+mise exec -- mix setup
+mise exec -- mix build
