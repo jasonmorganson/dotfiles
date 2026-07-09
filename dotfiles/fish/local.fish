54630d4c13835d2b3993b8301984e21a982a015d
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-09 18:51:29 -0400

    Prefer mise bootstrap shell activation

---
+++ dotfiles/fish/local.fish
@@ -0,0 +1,4 @@
+mise exec fnox -- fnox activate fish | source
+mise exec pitchfork -- pitchfork activate fish | source
+mise exec github:max-sixty/worktrunk -- wt config shell init fish | source
+mise exec starship -- starship init fish | source
