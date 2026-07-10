9c2920f1fbf142c8c65998be0d0141f773f46522
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-09 22:16:06 -0400

    Compile shell rc fragments during bootstrap

--- dotfiles/fish/local.fish
+++
@@ -1,6 +0,0 @@
-set -gx LS_COLORS (vivid --color-mode 8-bit generate one-dark)
-
-mise exec fnox -- fnox activate fish | source
-mise exec pitchfork -- pitchfork activate fish | source
-mise exec github:max-sixty/worktrunk -- wt config shell init fish | source
-mise exec starship -- starship init fish | source
