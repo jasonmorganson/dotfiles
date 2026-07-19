dffd683b60961b77f6d91c43100426afa19bd5cb
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-19 19:21:47 -0400

    Simplify mise install adapter

--- install.sh
+++ install.sh
@@ -2,13 +2,6 @@
 
 set -eu
 
-case $0 in
-    */*) self=$0 ;;
-    *) self=$(command -v "$0") ;;
-esac
-
-repo_dir="$(CDPATH= cd -P "$(dirname "$self")" && pwd -P)"
-export MISE_GLOBAL_CONFIG_FILE="$repo_dir/home/.config/mise/config.toml"
-export MISE_GLOBAL_CONFIG_ROOT="$repo_dir"
-
-exec "$repo_dir/home/.local/bin/mise" -C "$repo_dir" bootstrap --yes --force-dotfiles
+CDPATH= cd -P "$(dirname "$0")"
+export MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" MISE_GLOBAL_CONFIG_ROOT="$PWD"
+exec ./home/.local/bin/mise bootstrap --yes --force-dotfiles
