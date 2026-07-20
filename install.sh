ac738ee99ceffdd69bacf18619073fa8f0d133ce
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 09:04:47 -0400

    Remove unused mise config root

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@
 cd "$(dirname "$0")" &&
     curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
     MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
-        MISE_GLOBAL_CONFIG_ROOT="$PWD" \
         /usr/local/bin/mise bootstrap --yes --force-dotfiles
