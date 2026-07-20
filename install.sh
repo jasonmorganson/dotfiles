1a6837d2692b9c5c08b5c0fd16f6afd6457eb980
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 12:54:24 -0400

    Keep bootstrap locked and single-config

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
 	MISE_GLOBAL_CONFIG_FILE="$(dirname "$0")/home/.config/mise/config.toml" \
+		MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+${MISE_IGNORED_CONFIG_PATHS}:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
 		/usr/local/bin/mise bootstrap --yes --force-dotfiles
