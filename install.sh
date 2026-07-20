0332a709f6f5105ab623ade3f0d550ac888508b6
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 09:03:07 -0400

    Match documented mise curl command

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd "$(dirname "$0")" &&
-    curl -fsSL https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
+    curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
     MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
         MISE_GLOBAL_CONFIG_ROOT="$PWD" \
         /usr/local/bin/mise bootstrap --yes --force-dotfiles
