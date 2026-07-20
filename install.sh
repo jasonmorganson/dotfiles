9c0dcf6cdd45b26cb5d4b6900ab2806a2f98be72
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 09:01:12 -0400

    Minimize setup script

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-set -eu
-
-CDPATH= cd -P "$(dirname "$0")"
-export MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
-    MISE_GLOBAL_CONFIG_ROOT="$PWD"
-curl -fsSL https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh
-exec /usr/local/bin/mise bootstrap --yes --force-dotfiles
+cd "$(dirname "$0")" &&
+    curl -fsSL https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
+    MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
+        MISE_GLOBAL_CONFIG_ROOT="$PWD" \
+        /usr/local/bin/mise bootstrap --yes --force-dotfiles
