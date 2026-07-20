00a34a6174d33f7deb9daccdb5ba28155fedc88c
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 09:06:09 -0400

    Format installer with shfmt

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd "$(dirname "$0")" &&
-    curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
-    MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
-        /usr/local/bin/mise bootstrap --yes --force-dotfiles
+	curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
+	MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
+		/usr/local/bin/mise bootstrap --yes --force-dotfiles
