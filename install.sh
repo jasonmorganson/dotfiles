b1d8738a8b2d6c4e461ffa247a9409a44ebe75f0
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 08:59:18 -0400

    Simplify mise installer invocation

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ set -eu
 
 CDPATH= cd -P "$(dirname "$0")"
 export MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
-    MISE_GLOBAL_CONFIG_ROOT="$PWD" \
-    MISE_INSTALL_PATH=/usr/local/bin/mise
-curl -fsSL https://mise.run | sudo --preserve-env=MISE_INSTALL_PATH sh
+    MISE_GLOBAL_CONFIG_ROOT="$PWD"
+curl -fsSL https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh
 exec /usr/local/bin/mise bootstrap --yes --force-dotfiles
