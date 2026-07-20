ecad430542acf7fe998cf16e551a540e1bea8e12
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-20 09:09:16 -0400

    Avoid changing directory in installer

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd "$(dirname "$0")" &&
-	curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
-	MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
+curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
+	MISE_GLOBAL_CONFIG_FILE="$(dirname "$0")/home/.config/mise/config.toml" \
 		/usr/local/bin/mise bootstrap --yes --force-dotfiles
