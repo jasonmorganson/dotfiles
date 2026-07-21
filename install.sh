4bfa21fa4ad10d07c21a0ab32cff6669472650f4
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-21 15:42:17 -0400

    Organize mise configuration

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$0")"
 curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh
-MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
+MISE_CONFIG_DIR="$PWD/home/.config/mise" \
 MISE_DOTFILES_ROOT="$PWD/home" \
 MISE_IGNORED_CONFIG_PATHS="$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
 /usr/local/bin/mise bootstrap --yes --force-dotfiles
