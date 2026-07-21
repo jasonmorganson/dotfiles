35c715e47a8338ab341d154568333066ff7846b0
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-21 13:03:36 -0400

    Format installer

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 set -e
 
 cd "$(dirname "$0")"
-
 curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh
 MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
-	MISE_DOTFILES_ROOT="$PWD/home" \
-	MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+$MISE_IGNORED_CONFIG_PATHS:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
-	/usr/local/bin/mise bootstrap --yes --force-dotfiles
+MISE_DOTFILES_ROOT="$PWD/home" \
+MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+$MISE_IGNORED_CONFIG_PATHS:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
+/usr/local/bin/mise bootstrap --yes --force-dotfiles
