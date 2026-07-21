6ad890ceabb71c8f366cc137b94341d043d6d10b
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-21 12:56:23 -0400

    Simplify installer control flow

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 
-repo=$(cd "$(dirname "$0")" && pwd) || exit
+cd "$(dirname "$0")"
 
-curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
-	MISE_GLOBAL_CONFIG_FILE="$repo/home/.config/mise/config.toml" \
-		MISE_DOTFILES_ROOT="$repo/home" \
-		MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+${MISE_IGNORED_CONFIG_PATHS}:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
-		/usr/local/bin/mise bootstrap --yes --force-dotfiles
+curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh
+MISE_GLOBAL_CONFIG_FILE="$PWD/home/.config/mise/config.toml" \
+	MISE_DOTFILES_ROOT="$PWD/home" \
+	MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+$MISE_IGNORED_CONFIG_PATHS:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
+	/usr/local/bin/mise bootstrap --yes --force-dotfiles
