9c7074cc862e3fa1ce7ef25650dcb946805332d8
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-07-21 12:48:29 -0400

    Use checkout sources during bootstrap

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+repo=$(cd "$(dirname "$0")" && pwd) || exit
+
 curl https://mise.run | sudo env MISE_INSTALL_PATH=/usr/local/bin/mise sh &&
-	MISE_GLOBAL_CONFIG_FILE="$(dirname "$0")/home/.config/mise/config.toml" \
+	MISE_GLOBAL_CONFIG_FILE="$repo/home/.config/mise/config.toml" \
+		MISE_DOTFILES_ROOT="$repo/home" \
 		MISE_IGNORED_CONFIG_PATHS="${MISE_IGNORED_CONFIG_PATHS:+${MISE_IGNORED_CONFIG_PATHS}:}$HOME/.local/share/dotfiles/home/.config/mise/config.toml" \
 		/usr/local/bin/mise bootstrap --yes --force-dotfiles
