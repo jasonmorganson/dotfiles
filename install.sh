08322e665766af706ea1e86003169bffb6adcce0
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-08-25 17:53:59 -0400

    feat: bootstrap dotfiles from repository

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 set -e
 
 cd "$(dirname "$0")"
+if [ ! -d home/.config/mise ]; then
+    mkdir -p "$HOME/.local/share"
+    git clone "https://github.com/${GITHUB_USER:-jasonmorganson}/dotfiles.git" "$HOME/.local/share/dotfiles"
+    exec "$HOME/.local/share/dotfiles/install.sh"
+fi
+
 curl -fsSL https://mise.run | sh
 MISE_CONFIG_DIR="$PWD/home/.config/mise" \
 MISE_DOTFILES_ROOT="$PWD/home" \
