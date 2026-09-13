318e5cb93940099d6c6ef012450bb07d44104bae
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2026-09-13 09:17:47 -0400

    chore: use GitHub shorthand for mise adoption

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -e
 curl https://mise.run | sh
 export PATH="$HOME/.local/bin:$PATH"
 mise bootstrap \
-    --adopt "https://github.com/${GITHUB_USER:-jasonmorganson}/dotfiles.git" \
+    --adopt "${GITHUB_USER:-jasonmorganson}/dotfiles" \
     --update \
     --yes
